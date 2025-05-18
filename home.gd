9dbffb95cc8e5ed17803182dad87158a2a1a9671
RJ <rj@grandshipper.com>
2025-05-18 14:01:16 +0800

    fix game over layout

--- home.gd
+++ home.gd
@@ -18,6 +18,17 @@ func _on_hard_pressed() -> void:
 	GameState.hardLevel()
 	_on_play_pressed()
 
+
+func _on_random_pressed() -> void:
+	GameState.randomLevel()
+	_on_play_pressed()
+
+
+func _on_ai_pressed() -> void:
+	GameState.randomLevel()
+	_on_play_pressed()
+
+
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 	pass # Replace with function body.
